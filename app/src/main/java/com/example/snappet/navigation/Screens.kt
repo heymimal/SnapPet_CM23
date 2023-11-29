4bfaf2db621037912390c931ce764b107d3698c0
ricardo <ricardomcs2001@gmail.com>
2023-11-29 23:29:14 +0000

    Novas routes

--- app/src/main/java/com/example/snappet/navigation/Screens.kt
+++ app/src/main/java/com/example/snappet/navigation/Screens.kt
@@ -2,6 +2,9 @@ package com.example.snappet.navigation
 
 sealed class Screens(val route:String){
     object Home : Screens("home_route")
-    object Search : Screens("search_route")
+    object Map : Screens("map_route")
     object Profile : Screens("profile_route")
+    object Trophies: Screens("trophies_route")
+
+
 }
