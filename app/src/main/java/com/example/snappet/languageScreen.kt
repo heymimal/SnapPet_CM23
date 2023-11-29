d6f28ec2ac70252fa9bc8d1bf1e4906cfe508372
ricardo <ricardomcs2001@gmail.com>
2023-11-29 12:25:59 +0000

    Integrar NavBar no languageScreen

--- app/src/main/java/com/example/snappet/languageScreen.kt
+++ app/src/main/java/com/example/snappet/languageScreen.kt
@@ -1,18 +1,21 @@
 package com.example.snappet
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,83 +24,34 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomNavigationBar4() {
+    val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            Navigation(currentDestination = currentDestination, navController =navController)
+        }) {paddingValues ->
+        Text(text = "SnapPet Menu", modifier = Modifier.padding(paddingValues = paddingValues))
+        Language()
+
+        //Text(text = "Hello")
+    }
+}
+
 
 @Composable
 fun Language(modifier: Modifier = Modifier) {
     Box(
         modifier = modifier
-            .requiredWidth(width = 360.dp)
-            .requiredHeight(height = 800.dp)
+            .fillMaxSize()
     ) {
-        Box(
-            modifier = Modifier
-                .requiredWidth(width = 360.dp)
-                .requiredHeight(height = 800.dp)
-                .background(color = Color.White))
-        NavigationBar(
-            modifier = Modifier
-                .align(alignment = Alignment.TopStart)
-                .offset(x = 0.dp,
-                    y = 737.dp)
-        ) {
-            Box(
-                modifier = Modifier
-                    .requiredWidth(width = 360.dp)
-                    .requiredHeight(height = 63.dp)
-            ) {
-                Box(
-                    modifier = Modifier
-                        .align(alignment = Alignment.TopStart)
-                        .offset(x = 0.dp,
-                            y = 1.dp)
-                        .requiredWidth(width = 360.dp)
-                        .requiredHeight(height = 62.dp)
-                        .background(color = Color(0xffe99517).copy(alpha = 0.77f)))
-                Image(
-                    painter = painterResource(id = R.drawable.gameicon),
-                    contentDescription = "game",
-                    modifier = Modifier
-                        .align(alignment = Alignment.TopStart)
-                        .offset(x = 234.dp,
-                            y = 13.dp)
-                        .requiredWidth(width = 38.dp)
-                        .requiredHeight(height = 39.dp))
-                Image(
-                    painter = painterResource(id = R.drawable.homeicon),
-                    contentDescription = "home",
-                    modifier = Modifier
-                        .align(alignment = Alignment.TopStart)
-                        .offset(x = 9.dp,
-                            y = 13.dp)
-                        .requiredWidth(width = 50.dp)
-                        .requiredHeight(height = 42.dp))
-                Image(
-                    painter = painterResource(id = R.drawable.mapicon),
-                    contentDescription = "map",
-                    modifier = Modifier
-                        .align(alignment = Alignment.TopStart)
-                        .offset(x = 85.dp,
-                            y = 7.dp)
-                        .requiredSize(size = 50.dp))
-                Image(
-                    painter = painterResource(id = R.drawable.profileicon),
-                    contentDescription = "profile",
-                    modifier = Modifier
-                        .align(alignment = Alignment.TopStart)
-                        .offset(x = 298.dp,
-                            y = 7.dp)
-                        .requiredSize(size = 50.dp))
-                Image(
-                    painter = painterResource(id = R.drawable.cameraicon),
-                    contentDescription = "Camera",
-                    modifier = Modifier
-                        .align(alignment = Alignment.TopStart)
-                        .offset(x = 161.dp,
-                            y = 0.dp)
-                        .requiredWidth(width = 47.dp)
-                        .requiredHeight(height = 55.dp))
-            }
-        }
         Text(
             text = "Change Language",
             color = Color.Black,
@@ -105,13 +59,17 @@ fun Language(modifier: Modifier = Modifier) {
                 fontSize = 25.sp),
             modifier = Modifier
                 .align(alignment = Alignment.TopCenter)
-                .offset(x = 0.dp,
-                    y = 29.dp))
+                .offset(
+                    x = 0.dp,
+                    y = 29.dp
+                ))
         Box(
             modifier = Modifier
                 .align(alignment = Alignment.TopStart)
-                .offset(x = 26.dp,
-                    y = 146.dp)
+                .offset(
+                    x = 26.dp,
+                    y = 146.dp
+                )
                 .requiredWidth(width = 308.dp)
                 .requiredHeight(height = 61.dp)
         ) {
@@ -128,22 +86,28 @@ fun Language(modifier: Modifier = Modifier) {
                     fontSize = 25.sp),
                 modifier = Modifier
                     .align(alignment = Alignment.TopStart)
-                    .offset(x = 18.dp,
-                        y = 15.dp))
+                    .offset(
+                        x = 18.dp,
+                        y = 15.dp
+                    ))
             Image(
                 painter = painterResource(id = R.drawable.british),
                 contentDescription = "image 5",
                 modifier = Modifier
                     .align(alignment = Alignment.CenterStart)
-                    .offset(x = 244.dp,
-                        y = (-0.5).dp)
+                    .offset(
+                        x = 244.dp,
+                        y = (-0.5).dp
+                    )
                     .requiredSize(size = 50.dp))
         }
         Box(
             modifier = Modifier
                 .align(alignment = Alignment.TopStart)
-                .offset(x = 28.dp,
-                    y = 266.dp)
+                .offset(
+                    x = 28.dp,
+                    y = 266.dp
+                )
                 .requiredWidth(width = 308.dp)
                 .requiredHeight(height = 61.dp)
         ) {
@@ -160,22 +124,28 @@ fun Language(modifier: Modifier = Modifier) {
                     fontSize = 25.sp),
                 modifier = Modifier
                     .align(alignment = Alignment.TopStart)
-                    .offset(x = 16.dp,
-                        y = 15.dp))
+                    .offset(
+                        x = 16.dp,
+                        y = 15.dp
+                    ))
             Image(
                 painter = painterResource(id = R.drawable.portuguese),
                 contentDescription = "image 6",
                 modifier = Modifier
                     .align(alignment = Alignment.CenterStart)
-                    .offset(x = 242.dp,
-                        y = (-0.5).dp)
+                    .offset(
+                        x = 242.dp,
+                        y = (-0.5).dp
+                    )
                     .requiredSize(size = 50.dp))
         }
         Box(
             modifier = Modifier
                 .align(alignment = Alignment.TopStart)
-                .offset(x = 28.dp,
-                    y = 386.dp)
+                .offset(
+                    x = 28.dp,
+                    y = 386.dp
+                )
                 .requiredWidth(width = 308.dp)
                 .requiredHeight(height = 61.dp)
         ) {
@@ -192,22 +162,28 @@ fun Language(modifier: Modifier = Modifier) {
                     fontSize = 25.sp),
                 modifier = Modifier
                     .align(alignment = Alignment.TopStart)
-                    .offset(x = 17.dp,
-                        y = 16.dp))
+                    .offset(
+                        x = 17.dp,
+                        y = 16.dp
+                    ))
             Image(
                 painter = painterResource(id = R.drawable.spanish),
                 contentDescription = "image 7",
                 modifier = Modifier
                     .align(alignment = Alignment.CenterStart)
-                    .offset(x = 242.dp,
-                        y = (-0.5).dp)
+                    .offset(
+                        x = 242.dp,
+                        y = (-0.5).dp
+                    )
                     .requiredSize(size = 50.dp))
         }
         Box(
             modifier = Modifier
                 .align(alignment = Alignment.TopStart)
-                .offset(x = 26.dp,
-                    y = 640.dp)
+                .offset(
+                    x = 26.dp,
+                    y = 640.dp
+                )
                 .requiredWidth(width = 84.dp)
                 .requiredHeight(height = 52.dp)
         ) {
@@ -225,8 +201,10 @@ fun Language(modifier: Modifier = Modifier) {
                     fontSize = 20.sp),
                 modifier = Modifier
                     .align(alignment = Alignment.TopStart)
-                    .offset(x = 19.dp,
-                        y = 14.dp))
+                    .offset(
+                        x = 19.dp,
+                        y = 14.dp
+                    ))
         }
     }
 }
@@ -234,5 +212,5 @@ fun Language(modifier: Modifier = Modifier) {
 @Preview
 @Composable
 private fun LanguagePreview() {
-    Language(Modifier)
+    BottomNavigationBar4()
 }
