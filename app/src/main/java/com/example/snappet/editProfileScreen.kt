ceffe824294a6a7953125f1cb2b58157cdeab7d2
ricardo <ricardomcs2001@gmail.com>
2023-11-29 18:44:42 +0000

    Layout alterado no EditProfile

--- app/src/main/java/com/example/snappet/editProfileScreen.kt
+++ app/src/main/java/com/example/snappet/editProfileScreen.kt
@@ -1,18 +1,31 @@
 package com.example.snappet
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,21 +53,72 @@ fun editProfileNavBar() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditProfile(modifier: Modifier = Modifier) {
-    Box(
+    Column(
         modifier = modifier
             .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top
     ) {
         Text(
             text = "Edit Profile",
             color = Color.Black,
-            style = TextStyle(
-                fontSize = 25.sp),
+            style = TextStyle(fontSize = 25.sp),
             modifier = Modifier
-                .align(alignment = Alignment.TopCenter)
-                .offset(
-                    x = 0.dp,
-                    y = 29.dp
-                ))
+                .align(alignment = Alignment.CenterHorizontally)
+                .offset(y = 29.dp)
+        )
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+
+        Text(
+            text = "Profile Image",
+            color = Color.Black,
+            fontWeight = FontWeight.Bold,
+            style = TextStyle(fontSize = 20.sp)
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
+            //horizontalArrangement = Arrangement.SpaceBetween,
+            .padding(start = 0.dp, end = 16.dp),
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.Start
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.profilepic),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(120.dp)
+                    .clip(CircleShape)
+                    .padding(end = 16.dp)
+            )
+
+            Button(
+                onClick = { /* Ação do botão Update */ },
+                modifier = Modifier.padding(start = 16.dp)
+            ) {
+                Text(text = "Update")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        TextField(
+            value = "",
+            onValueChange = {/*TODO*/},
+            label = {Text("Username")},
+            modifier = Modifier.fillMaxWidth())
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        TextField(
+            value = "",
+            onValueChange = {/*TODO*/},
+            label = {Text("Password")},
+            modifier = Modifier.fillMaxWidth())
 
     }
 }
