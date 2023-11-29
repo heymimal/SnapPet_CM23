cf1dbbb6a7290e2381e4bfd9e0d3d738c01bf5c2
ricardo <ricardomcs2001@gmail.com>
2023-11-29 12:26:54 +0000

    Integrar NavBar no reportProblemScreen

--- app/src/main/java/com/example/snappet/reportProblemScreen.kt
+++ app/src/main/java/com/example/snappet/reportProblemScreen.kt
@@ -3,16 +3,19 @@ package com.example.snappet
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,84 +24,34 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomNavigationBar6() {
+    val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            Navigation(currentDestination = currentDestination, navController =navController)
+        }) {paddingValues ->
+        Text(text = "SnapPet Menu", modifier = Modifier.padding(paddingValues = paddingValues))
+        ReportProblem()
+
+        //Text(text = "Hello")
+    }
+}
 
 
 @Composable
 fun ReportProblem(modifier: Modifier = Modifier) {
     Box(
-        modifier = modifier
-            .requiredWidth(width = 360.dp)
-            .requiredHeight(height = 800.dp)
+        modifier = modifier.fillMaxSize()
     ) {
-        Box(
-            modifier = Modifier
-                .requiredWidth(width = 360.dp)
-                .requiredHeight(height = 800.dp)
-                .background(color = Color.White))
-        NavigationBar(
-            modifier = Modifier
-                .align(alignment = Alignment.TopStart)
-                .offset(x = 0.dp,
-                    y = 737.dp)
-        ) {
-            Box(
-                modifier = Modifier
-                    .requiredWidth(width = 360.dp)
-                    .requiredHeight(height = 63.dp)
-            ) {
-                Box(
-                    modifier = Modifier
-                        .align(alignment = Alignment.TopStart)
-                        .offset(x = 0.dp,
-                            y = 1.dp)
-                        .requiredWidth(width = 360.dp)
-                        .requiredHeight(height = 62.dp)
-                        .background(color = Color(0xffe99517).copy(alpha = 0.77f)))
-                Image(
-                    painter = painterResource(id = R.drawable.gameicon),
-                    contentDescription = "game",
-                    modifier = Modifier
-                        .align(alignment = Alignment.TopStart)
-                        .offset(x = 234.dp,
-                            y = 13.dp)
-                        .requiredWidth(width = 38.dp)
-                        .requiredHeight(height = 39.dp))
-                Image(
-                    painter = painterResource(id = R.drawable.homeicon),
-                    contentDescription = "home",
-                    modifier = Modifier
-                        .align(alignment = Alignment.TopStart)
-                        .offset(x = 9.dp,
-                            y = 13.dp)
-                        .requiredWidth(width = 50.dp)
-                        .requiredHeight(height = 42.dp))
-                Image(
-                    painter = painterResource(id = R.drawable.mapicon),
-                    contentDescription = "map",
-                    modifier = Modifier
-                        .align(alignment = Alignment.TopStart)
-                        .offset(x = 85.dp,
-                            y = 7.dp)
-                        .requiredSize(size = 50.dp))
-                Image(
-                    painter = painterResource(id = R.drawable.profileicon),
-                    contentDescription = "profile",
-                    modifier = Modifier
-                        .align(alignment = Alignment.TopStart)
-                        .offset(x = 298.dp,
-                            y = 7.dp)
-                        .requiredSize(size = 50.dp))
-                Image(
-                    painter = painterResource(id = R.drawable.cameraicon),
-                    contentDescription = "Camera",
-                    modifier = Modifier
-                        .align(alignment = Alignment.TopStart)
-                        .offset(x = 161.dp,
-                            y = 0.dp)
-                        .requiredWidth(width = 47.dp)
-                        .requiredHeight(height = 55.dp))
-            }
-        }
+
         Text(
             text = "Report Problem",
             color = Color.Black,
@@ -106,13 +59,17 @@ fun ReportProblem(modifier: Modifier = Modifier) {
                 fontSize = 25.sp),
             modifier = Modifier
                 .align(alignment = Alignment.TopCenter)
-                .offset(x = 0.5.dp,
-                    y = 26.dp))
+                .offset(
+                    x = 0.5.dp,
+                    y = 26.dp
+                ))
         Box(
             modifier = Modifier
                 .align(alignment = Alignment.TopStart)
-                .offset(x = 26.dp,
-                    y = 640.dp)
+                .offset(
+                    x = 26.dp,
+                    y = 640.dp
+                )
                 .requiredWidth(width = 84.dp)
                 .requiredHeight(height = 52.dp)
         ) {
@@ -130,14 +87,18 @@ fun ReportProblem(modifier: Modifier = Modifier) {
                     fontSize = 20.sp),
                 modifier = Modifier
                     .align(alignment = Alignment.TopStart)
-                    .offset(x = 18.dp,
-                        y = 14.dp))
+                    .offset(
+                        x = 18.dp,
+                        y = 14.dp
+                    ))
         }
         Box(
             modifier = Modifier
                 .align(alignment = Alignment.TopStart)
-                .offset(x = 246.dp,
-                    y = 640.dp)
+                .offset(
+                    x = 246.dp,
+                    y = 640.dp
+                )
                 .requiredWidth(width = 84.dp)
                 .requiredHeight(height = 52.dp)
         ) {
@@ -155,14 +116,18 @@ fun ReportProblem(modifier: Modifier = Modifier) {
                     fontSize = 20.sp),
                 modifier = Modifier
                     .align(alignment = Alignment.TopCenter)
-                    .offset(x = 1.dp,
-                        y = 14.dp))
+                    .offset(
+                        x = 1.dp,
+                        y = 14.dp
+                    ))
         }
         Box(
             modifier = Modifier
                 .align(alignment = Alignment.TopCenter)
-                .offset(x = 0.dp,
-                    y = 296.dp)
+                .offset(
+                    x = 0.dp,
+                    y = 296.dp
+                )
                 .requiredWidth(width = 332.dp)
                 .requiredHeight(height = 295.dp)
                 .background(color = Color(0xffd9d9d9)))
@@ -173,22 +138,26 @@ fun ReportProblem(modifier: Modifier = Modifier) {
                 fontSize = 20.sp),
             modifier = Modifier
                 .align(alignment = Alignment.TopStart)
-                .offset(x = 34.dp,
-                    y = 326.dp))
+                .offset(
+                    x = 34.dp,
+                    y = 326.dp
+                ))
         Image(
             painter = painterResource(id = R.drawable.problem),
             contentDescription = "image 12",
             modifier = Modifier
                 .align(alignment = Alignment.TopCenter)
-                .offset(x = 0.5.dp,
-                    y = 90.dp)
+                .offset(
+                    x = 0.5.dp,
+                    y = 90.dp
+                )
                 .requiredWidth(width = 177.dp)
                 .requiredHeight(height = 173.dp))
     }
 }
 
-@Preview(widthDp = 360, heightDp = 800)
+@Preview
 @Composable
 private fun ReportProblemPreview() {
-    ReportProblem(Modifier)
+    BottomNavigationBar6()
 }
